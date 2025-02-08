9317a97fa4ca1db9a6cc10871e72822310fb4a5c
Santi <canosantiago404@gmail.com>
2025-02-08 14:52:04 -0300

    feat: added controller and route for dashboard

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 /* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -19,9 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get("/hello", (req, res) => {
-  res.send("hello world");
-});
+app.use("/dashboard", dashboardRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3001;
